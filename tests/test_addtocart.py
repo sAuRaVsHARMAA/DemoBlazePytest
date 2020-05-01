401b50bfe47c1ba91eb721489cd0ea2a2a923c04
sAuRaVsHARMA <saurav.1300@gmail.com>
2020-05-01 14:55:08 +0530

    All Tests Passed With One Exception

--- tests/test_addtocart.py
+++ tests/test_addtocart.py
@@ -85,7 +85,6 @@ class TestAddToCart:
             print("Element not found ")
             screenshotname = moment.now().strftime("%d-%m-%Y_%H-%M-%S")
             allure.attach(self.driver.get_screenshot_as_png(), name=screenshotname, attachment_type=allure.attachment_type.PNG)
-
         except TimeoutException:
             print("Page load Timeout")
         except AssertionError:
